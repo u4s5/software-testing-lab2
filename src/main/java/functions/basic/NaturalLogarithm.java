e1a50decd91f0b953e1810173b1ab2682c9edc8f
u4s5 <u4s5@yandex.ru>
2017-04-08 02:40:25 +0300

    Add basic functions

---
+++ src/main/java/functions/basic/NaturalLogarithm.java
@@ -0,0 +1,38 @@
+package functions.basic;
+
+public class NaturalLogarithm {
+
+    private static final double PRECISION = 1e-5;
+
+    public static double calcLn(double x) {
+
+        if (x < 0D) {
+            throw new ArithmeticException("Impossible to calculate ln() of negative number");
+        }
+
+        if (x == 0D) {
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        if (x == Double.POSITIVE_INFINITY) {
+            return Double.POSITIVE_INFINITY;
+        }
+
+        double result = 0D;
+        final double a = (x - 1D) / (x + 1D);
+        double current;
+        double numerator = a;
+        int k = 1;
+
+        do {
+            current = numerator / k;
+            result += current;
+            k += 2;
+            numerator *= a * a;
+        } while (Math.abs(current) >= PRECISION / 100000);
+
+        return 2 * result;
+
+    }
+
+}
---
+++ src/main/java/functions/basic/Sine.java
@@ -0,0 +1,29 @@
+package functions.basic;
+
+public class Sine {
+
+    public static double calcSin(double arg) {
+
+        if (Double.isInfinite(arg)) {
+            throw new ArithmeticException("Impossible to calculate sin(infinity)");
+        }
+
+        if (Double.isNaN(arg)) {
+            return arg;
+        }
+
+        arg = arg % (2 * Math.PI);
+
+        double term = 1.0;
+        double sum = 0.0;
+
+        for (int i = 1; term != 0.0; i++) {
+            term *= arg / i;
+            sum += (~(i & 3) + 3) * (i & 1) * term;
+        }
+
+        return sum;
+
+    }
+
+}
